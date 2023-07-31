c6f70fea7a86160196165a427e9613e3a70f39c6
Deepak KK <dkk0011@gmail.com>
2023-07-31 17:34:24 +0530

    Integrated faked module

--- app/src/androidTest/java/com/deepak/currencyexchangeapp/FakeDatPrefModule.kt
+++ app/src/androidTest/java/com/deepak/currencyexchangeapp/FakeDatPrefModule.kt
@@ -1,9 +1,17 @@
 package com.deepak.currencyexchangeapp
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import com.deepak.currencyexchangeapp.di.DataStoreModule
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
 
 @Module
 @TestInstallIn(
@@ -11,4 +19,13 @@ import dagger.hilt.testing.TestInstallIn
     replaces = [DataStoreModule::class]
 )
 class FakeDatPrefModule {
+
+    @Provides
+    @Singleton
+    fun provideSkrapPref(@ApplicationContext context: Context): DataStore<Preferences> {
+        val dataStore = PreferenceDataStoreFactory.create {
+            context.preferencesDataStoreFile("app_pref")
+        }
+        return dataStore
+    }
 }
